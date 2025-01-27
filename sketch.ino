2c939724cceb4366166e25633c628b1d9e37dfd7
Alessandra1718 <92768931+Alessandra1718@users.noreply.github.com>
2025-01-27 09:00:52 -0800

    Add files via upload
    Uploaded the first version ( pseudocode ) of our arduino Sketch, this will be updated frequently

---
+++ sketch.ino
@@ -0,0 +1,23 @@
+void setup() {
+  //put your setup code here, to run once:
+  //set up pins and stuff 
+  // set up moisture sensor and add all other connections and power GND
+
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  // time sleep 1 hour - testing for 5 seconds or sm 
+  // check in on stuff 
+  // take measuemrnts and make a data table sample of differnt measurmemts 
+  // if else ect
+  // code in peices: 
+  // - pump running when called on ( function)
+  // - measurments of moisture sensor ( function separately )
+  // - add connections to wifi module/serial input -> later website 
+  // - lED light up (debug) or some sort of power mitigation circuit 
+  // code separately, add onto git or sm - > combine them together later into one mega file
+  // website and stuff 
+
+}

1ad5bbef81b7714c153d9a204fd244d51587a76c
Alessandra1718 <92768931+Alessandra1718@users.noreply.github.com>
2025-01-28 09:57:57 -0800

    Update sketch.ino

--- sketch.ino
+++ sketch.ino
@@ -10,11 +10,13 @@ void loop() {
   // connect the capacitative sensor
   // check in on plant and the readings 
   // large if statement ( can this be optimized?)(into a separate function?) on different readings in sensor : their results 
+  // this will need a separate dataset ( file?) to test different soil moisture levels and determine which is which 
   // take measurements and make a data table sample of differnt measurmemts 
-  // if else ect
   // code in peices: 
   // - pump running when called on (function)
   // - measurments of moisture sensor (function separately)
   // - add connections to wifi module/serial input -> later website 
   // - lED light up (debug) or some sort of power mitigation circuit 
+  //other platforms ( future) : add connection to serial print onto a website, add wifi/bluetooth module 
+  // add access for phones and other ways to see this information ( once the first is sucessfull) 
 }

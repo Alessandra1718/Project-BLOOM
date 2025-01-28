1d78122dbccddf45831f7508246a524341bb56a3
Alessandra1718 <92768931+Alessandra1718@users.noreply.github.com>
2025-01-28 09:56:19 -0800

    Update sketch.ino
    added further pseudocode thinking

--- sketch.ino
+++ sketch.ino
@@ -1,12 +1,15 @@
 void setup() {
   //set up pins and stuff 
   // set up moisture sensor, add all other connections and power GND
+  //capacitative moisture sensor ( ordered to come next week) and lcd screen library import HERE
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   // time sleep 1 hour - testing for 5 seconds  
-  // check in on stuff 
+  // connect the capacitative sensor
+  // check in on plant and the readings 
+  // large if statement ( can this be optimized?)(into a separate function?) on different readings in sensor : their results 
   // take measurements and make a data table sample of differnt measurmemts 
   // if else ect
   // code in peices: 

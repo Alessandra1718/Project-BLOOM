b223855e900d54af3458fc078a11433ba38a6478
Alessandra1718 <92768931+Alessandra1718@users.noreply.github.com>
2025-02-14 09:36:12 -0800

    Update Capacitative_moisture_sensor.ino
    Added parameters to function and made it more specific

--- Capacitative_moisture_sensor.ino
+++ Capacitative_moisture_sensor.ino
@@ -3,35 +3,42 @@ This will be a code for the moisture sensor - so that we are ready to connect it
 I will be coding this into a function so that I can simply add it into the main document 
 and call the function later ^^ 
 */
-// Define analog input
-#define sensorPin A0
 
-//(redo that part)^^^^
+// Define analog input- defined constants don't take up any memory on the arduino chip ( good for optimization)
+#define capSensorPin A0
+//capSensorPin is the name of the A0 output pin ( connected to the mositure sensor)
 
 void setup() {
       Serial.begin(9600);
-  //begin time 
+  //begin serial monitor ( might change the bits/sec)
+  //guide : https://docs.arduino.cc/language-reference/en/functions/communication/serial/begin/ 
 }
-//change this into a function 
 
 void loop() {
- soilMoistureSensor(parameterHere);
-//call this function 
-  
+ soilMoistureSensor(capSensorPin);
+//call this functionto hopefully run the sensor 
+  //I should later move/include the sleep (); and delay(); in this main loop instead 
+  //and make the long if loop/ operations from this function data -> watering pump connection 
 }
 
-void soilMoistureSensor(parameterHere){
+void soilMoistureSensor(sensorInput){
+  //this might be a bad parameter name but we can fix that later 
 //this functino will call the moistuer sensor 
   // change the parameter to include the pin of the moisture sensor 
-// 
 
   //take the data : 
-  int value = analogRead(sensorPin);
+  int value = analogRead(sensorInput);
+  //is it redundant to take data, store it in variable, and then show it in the serial monitor? 
+  // ( better to just call data from serial monitor directly? )
+  //^ this could be usefull when we transfer to bluetooth and have to send the data instead of the serial monitor 
+
+  //for now, this will be in this function, later we should take it out (change it )
 
   // Print the value to the serial monitor
   Serial.print("Analog output: ");
-  Serial.println(value);
-
+  Serial.print(value\n);
+  //print the value and then enter to next line
+      
   // set the machine to sleep for 2 seconds 
   // ( is this efficient? should I just put delay instead( what is the difference )? )
   sleep(2000);

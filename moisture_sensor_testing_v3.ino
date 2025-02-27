ec047230b6e51fb8dc2066780ba40fe87c104bab
Alessandra1718 <92768931+Alessandra1718@users.noreply.github.com>
2025-02-27 09:26:08 -0800

    Add files via upload
    Moisture sensor calibration and testing !
    (100-800 range of sensitivity)

---
+++ moisture_sensor_testing_v3.ino
@@ -0,0 +1,39 @@
+/*
+capcitiative soil moisture sensor testing and callibrating ( once we get it, this will be the first steps to our testing to make sure we know the 
+correct values for our sensor )
+*/
+
+//is it worth having this in a separate function for the testing code? should I just put this in the main loop to calibrate it 
+
+//internet sources: (calibration)
+#define capSensorPin A0
+
+void setup() {
+      Serial.begin(9600);
+      Serial.print("Hello! ");
+
+  //begin serial monitor ( might change the bits/sec)
+  //guide : https://docs.arduino.cc/language-reference/en/functions/communication/serial/begin/ 
+}
+
+void loop() {
+ soilMoistureSensor(capSensorPin);
+}
+
+void soilMoistureSensor(int sensorInput){
+
+  //take the data : 
+  int value = analogRead(sensorInput);
+
+  // Print the value to the serial monitor
+  Serial.println("Analog output: ");
+  Serial.println(value);
+  //print the value and then enter to next line
+
+//sleep is the most effiecnt -> it turns off the machine to low power mode ( but for the purposes of this testing we need to delay the program )
+  delay(2000);
+  //lower number means more moisture -> higher number means dryer moistures 
+
+  
+}
+
